4f88533a6e18db595c66a817fe442119b87a2809
ilkaay <ilkaay.yuksel@gmail.com>
2021-08-23 16:07:04 +0300

    Draggable and droppable list with react-beautiful-dnd package. Closes #1

--- src/components/container/toDoList/toDoList.tsx
+++ src/components/container/toDoList/toDoList.tsx
@@ -2,20 +2,47 @@ import "./toDoList.css";
 import ToDoListItem from "./toDoListItem/toDoListItem";
 import React, { useContext } from "react";
 import { TodosContext } from "../../../store/toDosContext";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ToDoList: React.FC = () => {
   const todosCtx = useContext(TodosContext);
 
+  const handeOnDrag = (result: any) => {
+    const items = Array.from(todosCtx.items);
+    const [renderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, renderedItem);
+    todosCtx.items = items;
+  };
+
   return (
-    <ul className='mt-5'>
-      {todosCtx.items.map((item) => (
-        <ToDoListItem
-          key={item.id}
-          text={item.text}
-          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
-        />
-      ))}
-    </ul>
+    <DragDropContext onDragEnd={handeOnDrag}>
+      <Droppable droppableId='items'>
+        {(provided) => (
+          <ul
+            className='mt-5'
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {todosCtx.items.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <ToDoListItem
+                      text={item.text}
+                      onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+                    />
+                  </li>
+                )}
+              </Draggable>
+            ))}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 export default ToDoList;
